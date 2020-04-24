1ee20de973269a7e336c12940140947abaa46be4
Baha eddine chammakhi <bahachammakhi25@gmail.com>
2020-04-24 02:37:11 +0100

    part6

--- src/containers/WeatherDetails.js
+++ src/containers/WeatherDetails.js
@@ -2,20 +2,26 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import TodayWeather from "../components/TodayWeather";
 import axios from "axios";
+import WeatherDays from "../components/WeatherDays";
 
 const WeatherDetails = () => {
   const { city } = useParams();
   const [loading, setLoading] = useState(true);
+  const [daylist, setDaylist] = useState([]);
   const [data, setData] = useState();
   const [error, setError] = useState(false);
   const apikey = "3d276dd0248a8f6d4a15500dc0dec11a";
+
+  const WeatherDaysList = daylist.map((el) => {
+    return <WeatherDays data={el} />;
+  });
   const getEveryDayFirst3hoursWeather = (array) => {
     let count = 0;
     let result = [];
     array.forEach((element, index) => {
       if (index - 8 === count || (count === 0 && index === 0)) {
         count = index;
-        result.push({ element, index });
+        result.push(element);
       }
     });
     return result;
@@ -26,8 +32,7 @@ const WeatherDetails = () => {
         `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}&units=metric`
       )
       .then((data) => {
-        console.log("data", data);
-        console.log("Result", getEveryDayFirst3hoursWeather(data.data.list));
+        setDaylist(getEveryDayFirst3hoursWeather(data.data.list));
         setData(data.data);
         setLoading(false);
         setError(false);
@@ -45,7 +50,7 @@ const WeatherDetails = () => {
         <h1>Wrong city or connection problem</h1>
       ) : (
         <div>
-          <TodayWeather data={data} />{" "}
+          <TodayWeather data={data} /> {WeatherDaysList}
         </div>
       )}
     </div>
