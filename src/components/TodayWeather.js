222686be4e2f09239fcf81ca1bbc8898bfd1aaf2
Baha eddine chammakhi <bahachammakhi25@gmail.com>
2020-04-24 02:24:29 +0100

    part6

--- src/components/TodayWeather.js
+++ src/components/TodayWeather.js
@@ -1,17 +1,24 @@
 import React from "react";
 
-const TodayWeather = () => {
+const TodayWeather = ({ data }) => {
+  const { country, name, population } = data?.city;
+  const today = data.list[0];
+  const { temp, humidity } = today.main;
+  const { description, icon } = today.weather[0];
   return (
     <div style={{ display: "flex", width: "80vw", padding: "10vw" }}>
       <div style={{ display: "flex", flexDirection: "column", width: "30vw" }}>
-        <h2>tunis</h2>
-        <span> country</span>
-        <p style={{ color: "gray" }}>description</p>
-        <p>Humidity : 70</p>
+        <h2>{name}</h2>
+        <span> {country}</span>
+        <p style={{ color: "gray" }}>{description}</p>
+        <p>Humidity : {humidity}</p>
       </div>
       <div style={{ width: "30vw" }}>
-        <img src="" alt="icon" />
-        <p style={{ fontWeight: "bolder" }}>17</p>
+        <img
+          src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+          alt="icon"
+        />
+        <p style={{ fontWeight: "bolder" }}>{temp}°</p>
       </div>
     </div>
   );
--- src/containers/WeatherDetails.js
+++ src/containers/WeatherDetails.js
@@ -9,20 +9,32 @@ const WeatherDetails = () => {
   const [data, setData] = useState();
   const [error, setError] = useState(false);
   const apikey = "3d276dd0248a8f6d4a15500dc0dec11a";
+  const getEveryDayFirst3hoursWeather = (array) => {
+    let count = 0;
+    let result = [];
+    array.forEach((element, index) => {
+      if (index - 8 === count || (count === 0 && index === 0)) {
+        count = index;
+        result.push({ element, index });
+      }
+    });
+    return result;
+  };
   useEffect(() => {
     axios
       .get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}&units=metric`
       )
       .then((data) => {
         console.log("data", data);
+        console.log("Result", getEveryDayFirst3hoursWeather(data.data.list));
         setData(data.data);
         setLoading(false);
         setError(false);
       })
       .catch((e) => {
-        setLoading(false);
         setError(true);
+        setLoading(false);
       });
   }, []);
   return (
@@ -33,7 +45,7 @@ const WeatherDetails = () => {
         <h1>Wrong city or connection problem</h1>
       ) : (
         <div>
-          <TodayWeather />{" "}
+          <TodayWeather data={data} />{" "}
         </div>
       )}
     </div>
